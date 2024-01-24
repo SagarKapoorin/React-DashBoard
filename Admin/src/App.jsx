05e66f1038e0be281e0a8fbdf93e964599246e85
Sagar Kapoor <sagarbadal70@gmail.com>
2024-01-24 19:30:40 +0530

    app+theme

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
-  return (
-    <>
+import { ColorModeContext,useMode } from "./theme"
+import { CssBaseline,ThemeProvider } from "@mui/material"
 
-    </>
+function App() {
+  const[theme,colorMode]=useMode();
+  return (
+    // colormode in response to context to provide to it
+    <ColorModeContext.Provider value={colorMode}>
+      {/* themeprovider mui's theme setting to give mui access */}
+      <ThemeProvider theme={theme}>
+        {/* cssbaseline reset any default css */}
+        <CssBaseline/>
+      <div className="app">
+        <main className="content">Hello</main>
+      </div>
+      </ThemeProvider>
+      </ColorModeContext.Provider>
   )
 }
 
